e5b263d685235f9fcd006b11fd34d2801c41e566
evbabin <evanbabn@gmail.com>
2024-05-06 08:19:35 -0700

    update

--- eSportsWiki/src/main/java/com/Starfox/EsportsWiki/controller/WebController.java
+++ eSportsWiki/src/main/java/com/Starfox/EsportsWiki/controller/WebController.java
@@ -1,10 +1,30 @@
 package com.Starfox.EsportsWiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.Starfox.EsportsWiki.service.CurrentTeamService;
+import com.Starfox.EsportsWiki.service.MatchService;
+
+import integrationAPI.UpdateDatabase;
 @Controller
 public class WebController {
+
+    MatchService matchService;
+    CurrentTeamService currentTeamService;
+
+    @Autowired
+    public WebController(MatchService matchService, CurrentTeamService currentTeamService){
+        this.matchService = matchService;
+        this.currentTeamService = currentTeamService;
+    }
+
     @GetMapping("/teams")
     public String teams(){
         return "teams";
@@ -21,7 +41,14 @@ public class WebController {
     }
 
     @GetMapping("/")
-    public String home(){
+    public String home(Model model) throws IOException, InterruptedException, URISyntaxException{
+        UpdateDatabase.updateTable("runningMatchList", "https://api.pandascore.co/matches/running" );
+        model.addAttribute("runningMatchDtoList", matchService.findAllRunningMatches());
+        model.addAttribute("lolMatchDtoList", matchService.findAllLolPastMatches());
+        model.addAttribute("csMatchDtoList", matchService.findAllCsPastMatches());    
+        model.addAttribute("codMatchDtoList", matchService.findAllCodPastMatches());
+        model.addAttribute("valMatchDtoList", matchService.findAllValPastMatches());
+        model.addAttribute("currentTeamDtoList", currentTeamService.findAllTeams()) ;   
         return "home";
     }
 
